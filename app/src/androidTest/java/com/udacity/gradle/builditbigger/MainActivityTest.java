b33e8833ad0f8b2368a5635dbb50cdd21d0115dd
Anna Myshliakova <ganna.myshliakova@gmail.com>
2018-08-06 15:29:18 -0700

    Code cleanup

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -2,18 +2,12 @@ package com.udacity.gradle.builditbigger;
 
 import android.app.Activity;
 import android.app.Instrumentation;
-import android.content.Intent;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingRegistry;
-import android.support.test.espresso.IdlingResource;
-import android.support.test.espresso.ViewInteraction;
+
 import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.android.jokedisplay.JokeDisplay;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -27,27 +21,26 @@ import static android.support.test.espresso.intent.Intents.intending;
 
 import static android.support.test.espresso.intent.matcher.BundleMatchers.hasEntry;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtras;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-
-
 import static org.hamcrest.core.AllOf.allOf;
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
+
+
+/*
+Tests that Async task successfully retrieves a non-empty string and sends it as an extra
+to the next Activity
+ */
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
     @Rule
-    //public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
     public IntentsTestRule<MainActivity> mActivityTestRule = new IntentsTestRule<>(MainActivity.class);
-
-    //private static final String JOKE = "This is a joke from jokeSource lib";
 
 
     @Before
@@ -63,13 +56,10 @@ public class MainActivityTest {
         intended(allOf(
                 hasComponent(JokeDisplay.class.getName()),
                 hasExtras(
-                        hasEntry(equalTo("joke_extra"), is(not(equalTo("")))))
+                        hasEntry(equalTo("joke_extra"), not(equalTo(""))))
                 )
         );
 
-
         //onView(withId(R.id.joke_textView)).check(matches(not(withText(""))));
-
     }
-
 }
