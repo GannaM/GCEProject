3a3362317194a375f4bde17b6884aade4656b640
Anna Myshliakova <ganna.myshliakova@gmail.com>
2018-08-07 14:19:34 -0700

    Relocated ad string resource into Free directory

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,44 @@
+package com.udacity.gradle.builditbigger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(JUnit4.class)
+public class EndpointsAsyncTaskTest {
+
+    @Test
+    public void testAsyncTaskDeliversJoke() throws Throwable {
+
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        final EndpointsAsyncTask myTask = new EndpointsAsyncTask(new EndpointsAsyncTask.AsyncResponse() {
+            @Override
+            public void fetchResult(String result) {
+
+                assertThat(result, not(isEmptyString()));
+
+                signal.countDown();
+            }
+
+        });
+
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                myTask.execute();
+            }
+        });
+
+        signal.await(30, TimeUnit.SECONDS);// wait for callback
+    }
+}
